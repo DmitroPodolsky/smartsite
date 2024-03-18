bf9fefde1e705d0f110575630234a6cf05f21809
DmitroPodolsky <podolskijdmitro1@gmail.com>
2024-03-19 00:07:27 +0200

    :art: style(Imports)

--- src/db/migrations/env.py
+++ src/db/migrations/env.py
@@ -5,7 +5,7 @@ from sqlalchemy import pool
 
 from alembic import context
 
-from models import *
+from db.models import Base
 from src.config import settings
 
 # this is the Alembic Config object, which provides
