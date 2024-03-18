7151f325d52432eaaa12452b94970271a05945cf
DmitroPodolsky <podolskijdmitro1@gmail.com>
2024-03-19 00:08:21 +0200

    :recycle: refactor: change paths

--- src/db/auto_migrate.py
+++ src/db/auto_migrate.py
@@ -1,7 +1,7 @@
 from alembic.config import Config
 from alembic import command
+from src.config import settings
 
-alembic_cfg = Config("./alembic.ini")
-
+alembic_cfg = Config(settings.DB_PATH / "alembic.ini")
 command.revision(alembic_cfg, autogenerate=True)
 command.upgrade(alembic_cfg, "head")
